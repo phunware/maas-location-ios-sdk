4068967a41faa0fd06e2faf9e58963831b72825c
Kent Tu <ktu@phunware.com>
2022-01-07 17:27:33 -0800

    Add dummy modules to pull in PWCore automagically.

--- Package.swift
+++ Package.swift
@@ -10,17 +10,25 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PWLocation",
-            targets: ["PWLocation"])
+            targets: ["PWLocationTargets"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url:"https://github.com/phunware/maas-core-ios-sdk.git", .branch("task/3.12.0-spm"))
+        .package(name:"PWCore", url:"https://github.com/phunware/maas-core-ios-sdk.git", .branch("task/3.12.0-spm"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         .binaryTarget(
             name: "PWLocation",
             path: "FrameworksStaticLinks/PWLocation.xcframework"
+        ),
+        .target(name: "PWLocationTargets",
+            dependencies:[
+                .target(name: "PWLocation"),
+                .product(name: "PWCore", package: "PWCore", condition: nil),
+                .product(name: "DeviceIdentity", package: "PWCore", condition: nil)
+            ],
+            path: "PWLocationTargets"
         )
     ]
 )
