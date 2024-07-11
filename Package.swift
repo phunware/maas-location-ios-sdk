43a42ef448c3a873bee35f82b57bccd77aeedf10
jenkins <hostmaster@phunware.com>
2024-07-11 15:25:17 -0700

    Update version 3.14.0. See CHANGELOG for details.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "PWLocation",
     platforms: [
-        .iOS(.v13)
+        .iOS("15.5")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name:"PWCore", url:"https://github.com/phunware/maas-core-ios-sdk.git", from: "3.12.2")
+        .package(url:"https://github.com/phunware/maas-core-ios-sdk.git", from: "3.13.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
